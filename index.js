03ab6d03ce823a1aa3a2cc8dc35c25aa865313be
ckiroshan <ckiroshan@gmail.com>
2025-08-13 15:26:11 +0530

    feat: install dotenv to store keys & config values

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
 app.use(cors());
 
@@ -8,4 +10,5 @@ app.get("/api/test", (req, res) => {
   res.json("Server is up & running!");
 });
 
-app.listen(8021);
+const PORT = process.env.PORT || 8028; // Server port
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
