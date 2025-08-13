5fa412465f63df46514ecf5bb44a3762202a412d
ckiroshan <ckiroshan@gmail.com>
2025-08-13 16:33:20 +0530

    feat: install mongoose & connect database to server

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js"; 
 import testRoutes from "./routes/testRoutes.js";
 
 dotenv.config(); // Loads env variables
+connectDB(); // Calls function to connect the database
 const app = express(); // Express app instance
 app.use(cors()); // CORS Middleware
 
