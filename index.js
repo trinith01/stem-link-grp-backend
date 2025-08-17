322a64664b2d361dc1c629faee7c0c06d014a25e
ckiroshan <ckiroshan@gmail.com>
2025-08-17 14:23:13 +0530

    feat: integrate Clerk middleware to secure API routes

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js"; 
 import testRoutes from "./routes/testRoutes.js";
+import { clerkMiddleware } from "@clerk/express";
 
 dotenv.config(); // Loads env variables
 connectDB(); // Calls function to connect the database
 const app = express(); // Express app instance
 app.use(cors()); // CORS Middleware
+app.use(clerkMiddleware()); // Reads JWT token from request & sets auth object in request
 
 // Routes
 app.use("/api", testRoutes);
