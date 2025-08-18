e76cfbb00c15df35ba43e26307ebff96b6ab18f7
ckiroshan <ckiroshan@gmail.com>
2025-08-18 07:26:02 +0530

    feat: implement isAuthenticated middleware for route protection & helpers

---
+++ middlewares/authentication-middleware.js
@@ -0,0 +1,18 @@
+import UnauthorizedError from "../domain/errors/unauthorized-error.js";
+
+// Custom authentication middleware using Clerk
+const isAuthenticated = (req, res, next) => {
+  console.log("IS_AUTHENTICATED", req.auth().isAuthenticated);
+  if (!req.auth().isAuthenticated) {
+    // Throw error if user is not authenticated
+    throw new UnauthorizedError("Unauthorized");
+  }
+  next();
+};
+
+// Helper to get user's Clerk userId from request
+export const getCurrentUserId = (req) => {
+  return req.auth().userId;
+};
+
+export { isAuthenticated, getCurrentUserId };
